63df17506d0c221c9ba6e1831f9af4f3aa722703
hymer <hymer2011@gmail.com>
2013-08-03 21:07:00 +0800

    create project.

---
+++ src/main/java/org/hymer/sensitivewords/App.java
@@ -0,0 +1,13 @@
+package org.hymer.sensitivewords;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
